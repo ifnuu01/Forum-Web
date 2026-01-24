75263622548dada7a54561fa68cd1781cf02f1b2
ifnuu01 <ifnuu01@gmail.com>
2026-01-24 20:56:00 +0800

    feat: add navigation and redux store

---
+++ src/navigation/Router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from '../pages/Home'
+import DetailThreadPage from '../pages/DetailThread'
+import LeaderboardPage from '../pages/Leaderboard'
+import ProfilePage from '../pages/Profile'
+
+export default function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/detail-thread' element={<DetailThreadPage />} />
+                <Route path='/papan-peringkat' element={<LeaderboardPage />} />
+                <Route path='/profile' element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
---
+++ src/store/store.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import threadsReducer from '../features/threads/threadsSlice';
+import commentsReducer from '../features/comments/commentsSlice';
+import leaderboardReducer from '../features/leaderboard/leaderboardSlice';
+import votesReducer from '../features/votes/votesSlice';
+import userReducer from '../features/user/userSlice';
+import replyHistoryReducer from '../features/replyHistory/replyHistorySlice';
+import createThreadReducer from '../features/createThread/createThreadSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    threads: threadsReducer,
+    comments: commentsReducer,
+    leaderboard: leaderboardReducer,
+    votes: votesReducer,
+    user: userReducer,
+    replyHistory: replyHistoryReducer,
+    createThread: createThreadReducer,
+  },
+});
+
+// Types untuk hooks
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
