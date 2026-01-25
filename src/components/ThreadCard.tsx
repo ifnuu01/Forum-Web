f0c4c5df644868fae43730286d5a864fc6dc783f
ifnuu01 <ifnuu01@gmail.com>
2026-01-25 15:43:15 +0800

    implement api integration get all threads in home page

--- src/components/ThreadCard.tsx
+++ src/components/ThreadCard.tsx
@@ -1,7 +1,47 @@
 import { ChevronRight, MessageSquare, SendHorizonal, ThumbsDown, ThumbsUp } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { timeAgo } from "../utils/fomat";
+import { useRef } from "react";
 
-export default function ThreadCard({ isDetail = false }: { isDetail?: boolean }) {
+export default function ThreadCard({
+    isDetail = false,
+    id,
+    title,
+    body,
+    category,
+    createdAt,
+    upVotesBy,
+    downVotesBy,
+    totalComments,
+    owner
+}: {
+    isDetail?: boolean,
+    id: string,
+    title: string,
+    body: string,
+    category: string,
+    createdAt: string,
+    avatar?: string,
+    ownerId?: string,
+    upVotesBy?: string[],
+    downVotesBy?: string[],
+    totalComments?: number,
+    owner?: {
+        id: string;
+        name: string;
+        avatar: string;
+    },
+}) {
+    const navigate = useNavigate();
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleTextAreaResize = () => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = "auto";
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        }
+    }
+
     return (
         <section
             className="w-full border-b-2 border-secondary px-8 pt-3 pb-2"
@@ -11,38 +51,39 @@ export default function ThreadCard({ isDetail = false }: { isDetail?: boolean })
                 <div className="order-2 md:order-1">
                     <div className="flex flex-col gap-2 md:items-center md:flex-row">
                         <h1>Dibuat oleh
-                            {isDetail && (
-                                <img
-                                    className="w-5 h-5 rounded-full object-cover inline ml-1"
-                                    src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                            )}
-                            <strong className="pl-1">Dimas</strong>
+                            <img
+                                className="w-5 h-5 rounded-full object-cover inline ml-1"
+                                src={owner?.avatar} alt="" />
+                            <strong className="pl-1">{owner?.name}</strong>
                         </h1>
-                        <p className="text-white/50">1 hari yang lalu</p>
+                        <p className="text-white/50">{timeAgo(createdAt)}</p>
                     </div>
                     <div>
-                        <Link to="/detail-thread" className="text-xl font-bold">Lorem ipsum dolar sit amet</Link>
+                        <Link to={`/detail-thread/${id}`} className="text-xl font-bold">{title}</Link>
                     </div>
                 </div>
                 <div className="px-4 w-fit rounded-full border-2 border-white order-1 mb-2">
-                    <span>Curhatan</span>
+                    <span>{category}</span>
                 </div>
             </div>
 
             {/* body */}
             <div className="mt-2">
-                <p>Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, qui....</p>
+                <p dangerouslySetInnerHTML={{ __html: body }}>
+                </p>
             </div>
 
             {/* footer */}
             <div className="flex items-center gap-4 mt-2 mb-1">
-                <button className="flex items-center gap-1 text-white/40"><ThumbsUp /><span>10</span></button>
-                <button className="flex items-center gap-1 text-white/40"><ThumbsDown /><span>0</span></button>
-                <button className="flex items-center gap-1 text-white/40"><MessageSquare /><span>0</span></button>
+                <button className="flex cursor-pointer hover:scale-110 transition-all duration-200 hover:text-white items-center gap-1 text-white/40"><ThumbsUp /><span>{upVotesBy ? upVotesBy.length : 0}</span></button>
+                <button className="flex cursor-pointer hover:scale-110 transition-all duration-200 hover:text-white items-center gap-1 text-white/40"><ThumbsDown /><span>{downVotesBy ? downVotesBy.length : 0}</span></button>
+                <button
+                    {...(isDetail ? {} : { onClick: () => navigate(`/detail-thread/${id}`) })}
+                    className="flex cursor-pointer hover:scale-110 transition-all duration-200 hover:text-white items-center gap-1 text-white/40"><MessageSquare /><span>{totalComments ? totalComments : 0}</span></button>
             </div>
 
             {isDetail && (
-                <form action="" className="flex justify-between items-center gap-4 mt-4">
+                <form action="" className="flex justify-between items-end gap-4 mt-4">
                     {/* profile */}
                     <div className="w-full">
                         <section className="flex items-center gap-2 mt-4 ">
@@ -54,13 +95,16 @@ export default function ThreadCard({ isDetail = false }: { isDetail?: boolean })
                             <span className="text-xs text-white/40">Komentar</span>
                         </section>
                         <textarea
+                            ref={textAreaRef}
+                            onInput={handleTextAreaResize}
+                            autoFocus
                             className="w-full mt-4 p-3 rounded-xl bg-primary focus:outline-none "
                             rows={1}
                             placeholder="Tulis komentar kamu disini..."
                         ></textarea>
                     </div>
                     <button className="bg-white text-primary rounded-full p-2 cursor-pointer hover:scale-110 transition-all duration-200">
-                        <SendHorizonal />
+                        <SendHorizonal className="rotate-270" />
                     </button>
                 </form>
             )}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,49 @@
 import ThreadCard from '../../components/ThreadCard';
 import Layout from '../../components/Layout';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch } from '../../store/store';
+import { fetchThreads, selectThreads } from '../../features/threads/threadsSlice';
+import { useEffect } from 'react';
+import { fetchAllUsers, selectUser } from '../../features/user/userSlice';
+import HomeSkeleton from './components/HomeSkeleton';
 
 export default function Home() {
-    // const dispatch = useDispatch<AppDispatch>();
-    // const { threads, loading, error } = useSelector(selectThreads);
+    const dispatch = useDispatch<AppDispatch>();
+    const { threads, loading, error } = useSelector(selectThreads);
+    const { users, loading: usersLoading, error: usersError } = useSelector(selectUser);
 
-    // useEffect(() => {
-    //     dispatch(fetchThreads());
-    // }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchThreads());
+        dispatch(fetchAllUsers());
+    }, [dispatch]);
 
-    // if (loading) return <div>Loading threads...</div>;
-    // if (error) return <div>Error: {error}</div>;
+    if (loading && usersLoading) {
+        return (
+            <Layout>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <HomeSkeleton key={index} />
+                ))}
+            </Layout>
+        )
+    }
+    if (error && usersError) return <div>Error: {error}</div>;
     return (
         <Layout>
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
-            <ThreadCard />
+            {threads.length > 0 ? threads.map((thread) => (
+                <ThreadCard
+                    key={thread.id}
+                    id={thread.id}
+                    title={thread.title}
+                    body={thread.body}
+                    category={thread.category}
+                    createdAt={thread.createdAt}
+                    owner={users.find(user => user.id === thread.ownerId)!}
+                    ownerId={thread.ownerId}
+                    upVotesBy={thread.upVotesBy}
+                    downVotesBy={thread.downVotesBy}
+                    totalComments={thread.totalComments}
+                />
+            )) : <div>No threads available.</div>}
         </Layout>
     );
 }
