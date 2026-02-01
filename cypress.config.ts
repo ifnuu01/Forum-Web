ea648640ccfd4fe736fd7ce18bc430cd4cc019f2
ifnuu01 <ifnuu01@gmail.com>
2026-02-01 16:43:32 +0800

    refactor

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5173',
+    defaultCommandTimeout: 10000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
