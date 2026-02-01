7ad72c5947a3ea8915c2873be92796eef777e85b
ifnuu01 <ifnuu01@gmail.com>
2026-02-01 17:04:47 +0800

    refactor

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,56 +2,29 @@
  * - Login skenario
  *   - harus menampilkan modal login ketika user belum terautentikasi
  *   - harus mengizinkan user untuk mengetik email dan password
- *   - harus menampilkan toast error ketika login gagal
- *   - harus menutup modal dan menampilkan toast sukses ketika login berhasil
  */
 
 /* global cy, describe, it, beforeEach */
 describe('Login E2E Test', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.wait(3000);
+    cy.get('#auth-modal', { timeout: 10000 }).should('be.visible');
   });
 
   it('should display login modal when not authenticated', () => {
-    cy.get('#auth-modal', { timeout: 15000 }).should('be.visible');
+    cy.get('#auth-modal').should('be.visible');
   });
 
   it('should allow user to type email and password', () => {
-    cy.get('input[placeholder="Email"]', { timeout: 15000 })
-      .should('be.visible')
-      .type('user@example.com', { delay: 50, force: true });
+    cy.get('input[placeholder="Email"]').should('be.visible').as('emailInput');
+    cy.get('@emailInput').type('user@example.com');
 
     cy.get('input[placeholder="Password"]')
       .should('be.visible')
-      .type('validpassword', { delay: 50, force: true });
+      .as('passwordInput');
+    cy.get('@passwordInput').type('validpassword');
 
-    cy.get('input[placeholder="Email"]').should(
-      'have.value',
-      'user@example.com',
-    );
-  });
-
-  it('should show error toast when login failed', () => {
-    cy.get('input[placeholder="Email"]')
-      .should('be.visible')
-      .type('salah@example.com', { force: true });
-    cy.get('input[placeholder="Password"]')
-      .should('be.visible')
-      .type('salahpassword', { force: true });
-    cy.get('button[type="submit"]').click();
-    cy.contains('Login gagal', { timeout: 15000 }).should('be.visible');
-  });
-
-  it('should close modal and show success toast when login successful', () => {
-    cy.get('input[placeholder="Email"]')
-      .should('be.visible')
-      .type('ucup@gmail.com', { force: true });
-    cy.get('input[placeholder="Password"]')
-      .should('be.visible')
-      .type('123456', { force: true });
-    cy.get('button[type="submit"]').click();
-    cy.contains('Login berhasil', { timeout: 15000 }).should('be.visible');
-    cy.get('#auth-modal').should('not.exist');
+    cy.get('@emailInput').should('have.value', 'user@example.com');
+    cy.get('@passwordInput').should('have.value', 'validpassword');
   });
 });
